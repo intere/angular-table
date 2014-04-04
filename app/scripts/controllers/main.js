29adc39de9f6eec97ba28b412c581281ba8ed1ac
Eric Internicola <einternicola@silverchalice.com>
2014-04-04 10:39:29 -0600

    master: Initial Commit - doesn't seem to work with Angular 1.2.6.  Let's see about 1.0.7.

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,26 @@
+'use strict';
+
+angular.module('angularTableApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.selectedRow = {};
+        $scope.listOfNumbers = [];
+
+        $scope.addRows = function(numberOfRowsToAdd) {
+            var startIndex = $scope.listOfNumbers.length;
+            var endIndex = $scope.listOfNumbers.length + numberOfRowsToAdd;
+
+            for(var i = startIndex; i < endIndex; i++) {
+                $scope.listOfNumbers.push({
+                   id: i,
+                   name: 'name ' + i,
+                   street: 'street ' + i
+                });
+            }
+        };
+
+        $scope.handleRowSelection = function(row) {
+            $scope.selectedRow = row;
+        };
+
+        $scope.addRows(50);
+  });
